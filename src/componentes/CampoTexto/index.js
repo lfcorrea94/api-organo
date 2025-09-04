331cec06dacd5b10ff7427740c6370682636f92e
lfcorrea94 <lfcorrea94@gmail.com>
2025-09-04 08:17:02 -0300

    dropdown list & nutton

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,12 +1,28 @@
 import './CampoTexto.css'
 
-const CampoTexto = (props) => {    
+const CampoTexto = (props) => {  
+    
+    const placeholderChanged = `${props.placeholder}...`
+
+    let valor = 'Luizão'
+
+    const aoDigitado = (evento) => {
+        valor = evento.target.value
+        console.log(valor)
+    }
+
+
     return(
         <div className="campo-texto">
             <label>
                 {props.label}
             </label>
-            <input placeholder={props.placeholder}/>
+            <input 
+                value={valor}
+                onChange={aoDigitado}
+                required={props.obrigatorio} 
+                placeholder={props.placeholder}
+            />
         </div>
     )
 }
